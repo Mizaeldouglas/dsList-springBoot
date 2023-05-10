d63a540e818992773e8631af5ead26798a1230ae
mizaeldouglas <mizaeldouglas2@gmail.com>
2023-05-09 21:54:37 -0300

    Properties, entidade Game, ORM

---
+++ src/main/java/br/com/mizaeldouglas/dsList/entities/Game.java
@@ -0,0 +1,38 @@
+package br.com.mizaeldouglas.dsList.entities;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.Objects;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "tb_game")
+public class Game {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    @Column(name = "game_year")
+    private Integer year;
+    private String genre;
+    private String platforms;
+    private String imgUrl;
+    private String shortDescription;
+    private String longDescription;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Game game)) return false;
+        return Objects.equals(getId(), game.getId());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId());
+    }
+}
