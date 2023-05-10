3a57670f3203f6e90d04e58af738d5eb12900b19
mizaeldouglas <mizaeldouglas2@gmail.com>
2023-05-09 21:14:02 -0300

    Project Created

---
+++ src/test/java/br/com/mizaeldouglas/dsList/DsListApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.mizaeldouglas.dsList;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
